757de8ca1f1291f2b61598c641934c0d8e54c818
Nicholas Luiz <nicholasnkzambom@gmail.com>
2022-11-10 22:03:29 -0300

    Classes models em criação

---
+++ src/java/controller/AlunoDAO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.Aluno;
+
+/**
+ *
+ * @author BINHO
+ */
+public class AlunoDAO {
+    
+    public  Aluno listarAluno(Aluno aluno){return aluno;}
+    
+    public void atualizarAluno (Aluno aluno){}
+    
+    public void matricularAluno (Aluno aluno){}
+    
+    public void transferirAluno (Aluno aluno){}
+    
+    public void remanejarAluno (Aluno aluno) {}
+    
+    public void deletarAluno (Aluno aluno) {}
+    
+    public void criarAluno(){}
+
+}
---
+++ src/java/controller/ClasseDAO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.Classe;
+
+/**
+ *
+ * @author BINHO
+ */
+public class ClasseDAO {
+    
+    public void criarClasse (){}
+    
+    public void atualizarClasse(Classe classe){}
+    
+    public void listarClasse(Classe classe){}
+    
+    public void deletarClasse(Classe classe){}
+}
